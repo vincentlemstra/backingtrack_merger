76d59a6b068c4e86a42e6b311a084f65a4456576
vincentlemstra <vincentlemstra@gmail.com>
2022-09-21 13:34:51 +0200

    add total audio overlay

--- merger.py
+++ merger.py
@@ -3,31 +3,22 @@ import glob
 from pydub import AudioSegment
 from pydub.utils import mediainfo
 
-# m4a_files = glob.glob(os.path.expanduser("~/Desktop/MULTITRACKS/*.m4a"))  # get all .m4a files
-# print(m4a_files)
+tracks = glob.glob(os.path.expanduser(
+    "~/Desktop/MULTITRACKS/*.m4a"))  # get all .m4a files
 
-# for wav_file in wav_files:
-#     mp3_file = os.path.splitext(wav_file)[0] + '.mp3' # rename to .mp3
-#     # print(mp3_file)
-#     sound = pydub.AudioSegment.from_wav(wav_file) # extract wav audio
-#     sound.export(mp3_file, format='mp3') # export into mp3 format
-#     # os.remove (wav_file)
-
-# print('Conversion Complete')
+setup_track = AudioSegment.from_file(os.path.expanduser(
+    "~/Desktop/MULTITRACKS/Click Track.m4a"), format="m4a")
+base_segment = AudioSegment.silent(
+    duration=len(setup_track))  # set empty track
+setup_bitrate = mediainfo(os.path.expanduser(
+    "~/Desktop/MULTITRACKS/Click Track.m4a"))["bit_rate"]  # setup bitrate
 
 
-# wav_files = glob.glob("./multitrack_files/*.wav")
-# print(wav_files) # You can use this to get a list -> get individual items by looping (instead of sound1 + sound2)
+for track in tracks:
+    audio = AudioSegment.from_file(track, format="m4a")  # extract m4a audio
+    base_segment = base_segment.overlay(audio)  # overlay audio
 
-sound1 = AudioSegment.from_file(os.path.expanduser("~/Desktop/MULTITRACKS/Click Track.m4a"), format="m4a")
-# sound2 = AudioSegment.from_file(os.path.expanduser("~/Desktop/MULTITRACKS/Guide.m4a"), format="m4a")
-sound3 = AudioSegment.from_file(os.path.expanduser("~/Desktop/MULTITRACKS/Drums (Live).m4a"), format="m4a")
 
-bitrate = mediainfo(os.path.expanduser("~/Desktop/MULTITRACKS/Click Track.m4a"))["bit_rate"]
-
-base_segment = AudioSegment.silent(duration=len(sound1))
-
-combined = base_segment.overlay(sound1)
-
-file_handle = combined.export("./combined_files/combined.mp3", format="mp3", bitrate=bitrate)
-print('Conversion Complete') 
+file_handle = base_segment.export("./combined_files/tracks_combined.mp3",
+                                  format="mp3", bitrate=setup_bitrate)  # export combined file
+print('Conversion Complete')
